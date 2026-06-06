6d81867991229d5445e8e3506456983270b90f5d
TheN1NJ4LL0 <It@floodman.com>
2026-06-05 20:15:24 -0400

    Rename plugin to chest_form_cpp to avoid prohibited endstone name prefix

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -122,7 +122,7 @@ private:
     std::unique_ptr<ChestFormListener> listener_;
 };
 
-ENDSTONE_PLUGIN("endstone_chest_form_cpp", "1.0.0", ChestFormPlugin) {
+ENDSTONE_PLUGIN("chest_form_cpp", "1.0.0", ChestFormPlugin) {
     prefix = "ChestFormPlugin";
     description = "Native C++ ChestFormAPI for fake chest inventory forms";
     website = "https://github.com/GlacieTeam/ChestFormAPI";
